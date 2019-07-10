bab24c79d4611339cc23f7001192bd3ba6be3c7f
Student Joost Bos <jbos1135@student.landstede.nl>
2019-07-10 11:43:45 +0200

    Remove git ignore

--- database/migrations/2019_07_10_074344_create_movies_table.php
+++ database/migrations/2019_07_10_074344_create_movies_table.php
@@ -16,7 +16,6 @@ class CreateMoviesTable extends Migration
         Schema::create('movies', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('title');
-            $table->bigInteger('genre_id')->unsigned();
             $table->integer('minutes');
             $table->longText('plot');
             $table->string('image');
