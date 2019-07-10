889bb0e8c3bd00c1bbe6225a02e03bcfa505b724
Student Joost Bos <jbos1135@student.landstede.nl>
2019-07-10 11:39:36 +0200

    foreignkey

---
+++ database/migrations/2019_07_10_093001_add_foreign_keys.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKeys extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('movies', function (Blueprint $table){
+            $table->bigInteger('genre_id')->unsigned();
+            $table->foreign('genre_id')->references('id')->on('genres');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
